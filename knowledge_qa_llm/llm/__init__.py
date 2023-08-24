c766ba9dd87f9c41a65200d84acc38f853529bf1
SWHL <liekkaskono@163.com>
2023-08-24 20:32:45 +0800

    Fix typo

--- knowledge_qa_llm/llm/__init__.py
+++ knowledge_qa_llm/llm/__init__.py
@@ -3,7 +3,7 @@
 # @Contact: liekkaskono@163.com
 from .baichuan_7b import BaiChuan7B
 from .chatglm2_6b import ChatGLM2_6B
-from .enrine_bot_turbo import ERNIEBotTurbo
+from .ernie_bot_turbo import ERNIEBotTurbo
 from .qwen7b_chat import Qwen7B_Chat
 
 __all__ = ["BaiChuan7B", "ChatGLM2_6B", "ERNIEBotTurbo", "Qwen7B_Chat"]
