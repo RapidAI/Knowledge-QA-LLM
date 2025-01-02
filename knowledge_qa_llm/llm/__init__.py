79f0bbcba9bfbd5774926155c4a1eb43ec8b8f4c
leo <leo@promptcn.com>
2025-01-02 12:45:35 +0800

    feat: add llm ollama

--- knowledge_qa_llm/llm/__init__.py
+++ knowledge_qa_llm/llm/__init__.py
@@ -7,5 +7,6 @@ from .ernie_bot_turbo import ERNIEBot
 from .internlm_7b import InternLM_7B
 from .qwen7b_chat import Qwen7B_Chat
 from .openai import OpenAI
+from .ollama import Ollama
 
-__all__ = ["BaiChuan7B", "ChatGLM2_6B", "ERNIEBot", "Qwen7B_Chat", "InternLM_7B", "OpenAI"]
+__all__ = ["BaiChuan7B", "ChatGLM2_6B", "ERNIEBot", "Qwen7B_Chat", "InternLM_7B", "OpenAI", "Ollama"]
